8e4dcc3daaca24334ec5ea26c6f2ff1db07e423e
Sabina Mlinarič <sabina.mlinaric@inova.si>
2017-02-09 12:57:00 +0100

    Project init

---
+++ SeeTest SparApp/SeeTest SparApp/Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using experitestClient;
+namespace Experitest
+{
+    [TestClass]
+    public class className
+    {
+        private String host = "localhost";
+        private int port = 8888;
+        private string projectBaseDirectory = "";
+        private string activeDevice = "";
+        protected Client client = null;
+
+        [TestInitialize()]
+        public void SetupTest()
+        {
+            client = new Client(host, port);
+            client.SetProjectBaseDirectory(projectBaseDirectory);
+            client.SetReporter("xml", "reports");
+        }
+
+        [TestMethod]
+        public void TestUntitled()
+        {
+            // it is recommanded to start your script with SetApplicationTitle command:
+            // client.SetApplicationTitle( activeDevice);
+        }
+        [TestCleanup()]
+        public void TearDown()
+        {
+            client.GenerateReport();
+        }
+    }
+}
