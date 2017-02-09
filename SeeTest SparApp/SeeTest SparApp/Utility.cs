f1551261917a78a3ce788a3c2782432c776d05ff
Sabina Mlinarič <sabina.mlinaric@inova.si>
2017-02-09 16:10:10 +0100

    a

--- SeeTest SparApp/SeeTest SparApp/Utility.cs
+++ SeeTest SparApp/SeeTest SparApp/Utility.cs
@@ -12,8 +12,10 @@ namespace SeeTest_SparApp.Utility
         public static readonly string DeviceName = "adb:D6603";
 	    public static readonly string Activity = "plus.spar.si/.ui.splash.SplashActivity";
 	    public static readonly string App = "plus.spar.si";
+        public static readonly string App2 = "plus.spar.si";
         public static readonly string projectBaseDirectory = "C:\\Users\\sabinam\\Google Drive\\project2";
         public static readonly string ddd = "C:\\Users\\sabinam\\Google Drive\\project2\\scenarios\\DDD\\Registration.csv";
+        
 
     }
 }
