9a9083da2c43cf401543e9f554ab621cc5066bdf
Sabina Mlinarič <sabina.mlinaric@inova.si>
2017-02-09 13:33:56 +0100

    Utility added and main folders

---
+++ SeeTest SparApp/SeeTest SparApp/Utility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using experitestClient;
+namespace Experitest
+{
+    public class Utility
+    {
+        public static string DeviceName = "adb:SM-J500FN";
+	    public static string Activity = "plus.spar.si/.ui.splash.SplashActivity";
+	    public static string App = "plus.spar.si";
+    }
+}
