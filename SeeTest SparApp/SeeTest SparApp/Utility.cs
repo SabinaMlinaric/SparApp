b018409e1950e025695697cde294ab386cde48b1
domenf <domenf@DOMENF-DESKTOPH>
2017-02-10 14:28:36 +0100

    Created JedelBi Class Add Code to SerialNumber Class Change my Utility class

--- SeeTest SparApp/SeeTest SparApp/Utility.cs
+++ SeeTest SparApp/SeeTest SparApp/Utility.cs
@@ -9,13 +9,11 @@ namespace SeeTest_SparApp.Utility
 {
     public class Utility
     {
-        public static readonly string DeviceName = "adb:D6603";
+        public static readonly string DeviceName = "adb:Sony";
 	    public static readonly string Activity = "plus.spar.si/.ui.splash.SplashActivity";
-	    public static readonly string App = "plus.spar.si";
+        public static readonly string App = "plus.spar.si";
         public static readonly string App2 = "plus.spar.si";
-        public static readonly string projectBaseDirectory = "C:\\Users\\sabinam\\Google Drive\\project2";
-        public static readonly string ddd = "C:\\Users\\sabinam\\Google Drive\\project2\\scenarios\\DDD\\Registration.csv";
-        
-
+        public static readonly string projectBaseDirectory = "C:\\Users\\domenf\\Google Drive\\Project2";
+        public static readonly string ddd = "C:\\Users\\domenf\\Google Drive\\project2\\scenarios\\DDD\\Registration.csv";
     }
 }
